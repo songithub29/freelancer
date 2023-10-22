b7c9fbef4e820c94e00d2d71df28e95026d40700
LeBaoTai <fnight644@gmail.com>
2023-10-22 12:20:33 +0700

    initalize project

---
+++ src/main/java/com/lbt/freelancer/FreelancerApplication.java
@@ -0,0 +1,12 @@
+package com.lbt.freelancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FreelancerApplication.class, args);
+  }
+}
---
+++ src/test/java/com/lbt/freelancer/FreelancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbt.freelancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
