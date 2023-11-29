3cbff1ad1d49ea53996afed3738efc121edf5d06
son chan <113790788+songithub29@users.noreply.github.com>
2023-11-29 13:56:58 +0700

    Update HomeController.java

--- src/main/java/com/lbt/freelancer/HomeController.java
+++ src/main/java/com/lbt/freelancer/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
   @GetMapping()
   public String home() {
-    return "dasdsa controller";
+    return "d controller";
   }
 }
